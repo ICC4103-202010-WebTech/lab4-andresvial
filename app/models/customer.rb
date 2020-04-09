046db073eefd3c3bf99b8dab8147efa2ee55916a
andresvial <avial5@miuandes.cl>
2020-04-09 12:59:22 -0400

    UPdated with callbacks and validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+  validates :name, :lastname, presence: true
+  validates :email, format: {with: /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
   has_many :orders
   has_many :tickets, through: :orders
 end
+
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,12 +2,27 @@ class Ticket < ApplicationRecord
   belongs_to :order
   belongs_to :ticket_type
 
-  # TODO: complete the folowing
-  # before_xxxxx :update_stats
+  before_create :increase_stats
+  after_destroy :decrease_stats
 
   private
-    def update_stats
-      es = self.ticket_type.event.event_stat
-      # TODO: complete in order to update event stats
+  def increase_stats
+    es = self.ticket_type.event.event_stat
+    ven = self.ticket_type.event.event_venue
+    if es.tickets_sold >= ven.capacity
+      raise Exceptions::CriticalError.new("something went wrong!")
+      return
     end
+    es.tickets_sold = es.tickets_sold + 1
+    es.attendance = es.attendance + 1
+    es.save
+  end
+
+  private
+  def decrease_stats
+    es = self.ticket_type.event.event_stat
+    es.tickets_sold = es.tickets_sold - 1
+    es.attendance = es.attendance - 1
+    es.save
+  end
 end
